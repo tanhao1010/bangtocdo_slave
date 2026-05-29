52b8393c451fc04a71f0450c64e98871fc6132d7
Haonguyen1010 <tanhaonguyen0402@gmail.com>
2026-05-29 10:44:01 +0700

    as

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ const char* WIFI_PASSWORD = "ATPro1234560";
 
 // 2) Version firmware hiện tại đang chạy trên ESP32.
 //    Mỗi lần build firmware mới -> tăng số này lên (vd 1 -> 2)
-const int FIRMWARE_VERSION = 2;
+const int FIRMWARE_VERSION = 3;
 
 // 3) URL raw trên GitHub (đổi USER/REPO/BRANCH cho đúng repo của bạn)
 //    File version.txt chỉ chứa 1 số nguyên, vd: 2
@@ -138,6 +138,6 @@ void setup() {
 }
 
 void loop() {
-   Serial.println("3");
+   Serial.println("4");
   delay(1000);
 }
